a025c75f357da09891fd204233de2142b364a23a
johannady2 <johannady2@gmail.com>
2026-05-09 15:16:03 +0900

    bcrypt compare when login

--- index.js
+++ index.js
@@ -80,27 +80,51 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const email = req.body.username;
-  const password = req.body.password;
+  const loginpassword = req.body.password;
 
-  try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      const storedPassword = user.password;
+  try
+  {
+          const result = await db.query("SELECT * FROM users WHERE email = $1", [
+            email,
+          ]);
+          if (result.rows.length > 0)
+          {
+                  const user = result.rows[0];
+                  const storedHashedPassword = user.password;
 
-      if (password === storedPassword) {
-        res.render("secrets.ejs");
-      } else {
-        res.send("Incorrect Password");
-      }
-    } else {
-      res.send("User not found");
-    }
-  } catch (err) {
-    console.log(err);
-  }
+
+                  bcrypt.compare(loginpassword, storedHashedPassword, (err, result) =>
+                  {
+                    if(err)
+                    {
+                      console.log("Error comparing passwords", err);
+                    }
+                    else
+                    {
+                      console.log(result);
+
+                      if(result)
+                      {
+                        res.render("secrets.ejs");
+                      }
+                      else
+                      {
+                        res.send("Incorrect Email or Password");
+                      }
+                    }
+                  });
+
+
+
+          } else 
+          {
+            res.send("User not found");
+          }
+        }
+        catch(err)
+        {
+          console.log(err);
+        }
 });
 
 app.listen(port, () => {
